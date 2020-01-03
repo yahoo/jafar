0a31f0e21a60a668894e82fa1cf0c1aec6720240
Gal Havivi <gal.havivi@oath.com>
2020-01-03 19:39:13 +0200

    Updates

---
+++ js/update-href-urls.js
@@ -0,0 +1,33 @@
+/**
+  * Copyright 2020, Verizon Media
+  * Licensed under the terms of the MIT license. See LICENSE file in project root for terms.
+  */
+
+// adding suffix of "index.html" to the header link,
+// because we refer to S3 and they will not auto trnslate the directory referer to inner index.html
+!(function () {
+  let isReady = false;
+  document.addEventListener('DOMContentLoaded', () => {
+    const header = document.getElementsByTagName('HEADER')[0];
+    const logo = header.childNodes[0];
+    const href = `${logo.href}index.html`;
+    logo.setAttribute('href', href);
+  });
+  window.addEventListener('message', (event) => {
+    if (isReady && event.data && typeof event.data === 'string' && event.data.indexOf('#') > -1) {
+      location.hash = event.data;
+    }
+  });
+  const syncLocationWithIframe = (event) => {
+    isReady = true;
+    const frames = document && document.getElementsByClassName('hosted-frame');
+    if (location && location.hash) {
+      Object.values(frames || []).forEach(frame => {
+        const host = frame.src.substr(0, frame.src.indexOf('html') + 4);
+        frame.src = `${host}${location.hash}`;
+      });
+    }
+  };
+  window.addEventListener('load', syncLocationWithIframe);
+  window.addEventListener('popstate', syncLocationWithIframe);
+}());
