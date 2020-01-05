9175b83f216591651cea0ddebb6adb316806d5e7
Gal Havivi <gal.havivi@oath.com>
2020-01-05 23:49:17 +0200

    Updates

---
+++ react-layout/precache-manifest.1ed030447438177371e1af2c11d248c1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd828992aafd1c51ef265e9254d5c22",
+    "url": "https://yahoo.github.io/jafar/react-layout/index.html"
+  },
+  {
+    "revision": "467e4d3a89db90e4f4b2",
+    "url": "https://yahoo.github.io/jafar/react-layout/static/css/main.efbfe3cd.css"
+  },
+  {
+    "revision": "467e4d3a89db90e4f4b2",
+    "url": "https://yahoo.github.io/jafar/react-layout/static/js/main.5f9b039c.js"
+  },
+  {
+    "revision": "1c1a52059bdc91a8a6e4ac28bea53710",
+    "url": "https://yahoo.github.io/jafar/react-layout/static/media/jafar.1c1a5205.svg"
+  }
+]);
