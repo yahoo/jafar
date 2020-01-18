3282fdf7959421fcf5028ef3fa7cdbe48cf14064
Travis CI <travis@travis-ci.org>
2020-01-18 18:33:34 +0000

    Updates

---
+++ demo-env/638892825/react-form/precache-manifest.d84f7ffe2ead6d3e1e878e82abd99056.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016fe0ae9da3e968feee5c90658c9152",
+    "url": "https://yahoo.github.io/jafar/demo-env/638892825/react-form/index.html"
+  },
+  {
+    "revision": "d222dde9a4a6dea6d1f2",
+    "url": "https://yahoo.github.io/jafar/demo-env/638892825/react-form/static/css/main.99903ae4.css"
+  },
+  {
+    "revision": "d222dde9a4a6dea6d1f2",
+    "url": "https://yahoo.github.io/jafar/demo-env/638892825/react-form/static/js/main.3d9e3a4a.js"
+  },
+  {
+    "revision": "1c1a52059bdc91a8a6e4ac28bea53710",
+    "url": "https://yahoo.github.io/jafar/demo-env/638892825/react-form/static/media/jafar.1c1a5205.svg"
+  }
+]);
