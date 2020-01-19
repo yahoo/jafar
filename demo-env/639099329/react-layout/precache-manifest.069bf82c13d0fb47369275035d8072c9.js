bc8c66e32a67b526f82fa58b59a78695ba279fc8
Travis CI <travis@travis-ci.org>
2020-01-19 12:21:51 +0000

    Updates

---
+++ demo-env/639099329/react-layout/precache-manifest.069bf82c13d0fb47369275035d8072c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03cb8e29834a174be726f9f17fd0b5e",
+    "url": "https://yahoo.github.io/jafar/demo-env/639099329/react-layout/index.html"
+  },
+  {
+    "revision": "6f2d2e934edb0ab5539f",
+    "url": "https://yahoo.github.io/jafar/demo-env/639099329/react-layout/static/css/main.99903ae4.css"
+  },
+  {
+    "revision": "6f2d2e934edb0ab5539f",
+    "url": "https://yahoo.github.io/jafar/demo-env/639099329/react-layout/static/js/main.234c6a87.js"
+  },
+  {
+    "revision": "1c1a52059bdc91a8a6e4ac28bea53710",
+    "url": "https://yahoo.github.io/jafar/demo-env/639099329/react-layout/static/media/jafar.1c1a5205.svg"
+  }
+]);
