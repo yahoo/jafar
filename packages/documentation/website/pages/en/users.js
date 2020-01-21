aa846349a698297e4dfbe063562f9a1eef57b3cf
Gal Havivi <gal.havivi@oath.com>
2020-01-21 23:01:15 +0200

    Update users.js

--- packages/documentation/website/pages/en/users.js
+++ packages/documentation/website/pages/en/users.js
@@ -18,7 +18,7 @@ class Users extends React.Component {
       return null;
     }
 
-    const editUrl = `${siteConfig.repoUrl}/edit/master/website/users.js`;
+    const editUrl = `${siteConfig.repoUrl}/blob/master/packages/documentation/website/users.js`;
     const showcase = siteConfig.users.map(user => (
       <a href={user.infoLink} key={user.infoLink}>
         <img src={user.image} alt={user.caption} title={user.caption} />
