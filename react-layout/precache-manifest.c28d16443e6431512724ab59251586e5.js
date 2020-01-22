01add35939c96f8417c550751d711012aba6a271
Travis CI <travis@travis-ci.org>
2020-01-22 18:39:24 +0000

    Updates

--- react-layout/precache-manifest.c28d16443e6431512724ab59251586e5.js
+++ react-layout/precache-manifest.c28d16443e6431512724ab59251586e5.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "44c74e530707c041cc18fc141f2f677b",
+    "revision": "18c42e20b04d7d287b0706fab0273299",
     "url": "https://yahoo.github.io/jafar/react-layout/index.html"
   },
   {
-    "revision": "7f1a4da7c9756fadefbd",
+    "revision": "1eae27ea703047404e2e",
     "url": "https://yahoo.github.io/jafar/react-layout/static/css/main.99903ae4.css"
   },
   {
-    "revision": "7f1a4da7c9756fadefbd",
-    "url": "https://yahoo.github.io/jafar/react-layout/static/js/main.44e8b514.js"
+    "revision": "1eae27ea703047404e2e",
+    "url": "https://yahoo.github.io/jafar/react-layout/static/js/main.11bfd9ea.js"
   },
   {
     "revision": "1c1a52059bdc91a8a6e4ac28bea53710",
--- react-layout/service-worker.js
+++ react-layout/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://yahoo.github.io/jafar/react-layout/precache-manifest.62cd7b2057cd148617604d6331f2b4cc.js"
+  "https://yahoo.github.io/jafar/react-layout/precache-manifest.c28d16443e6431512724ab59251586e5.js"
 );
 
 self.addEventListener('message', (event) => {
