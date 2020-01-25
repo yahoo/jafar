ca27995068119e84ca75acaf6bb307bdc674ded5
Gal Havivi <gal.havivi@oath.com>
2020-01-25 11:06:35 +0200

    update

--- packages/react-components/src/edit/Number/Number.jsx
+++ packages/react-components/src/edit/Number/Number.jsx
@@ -6,14 +6,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Input from '@material-ui/core/Input';
-import { propTypes, defaultProps } from '../props';
 
+/**
+ * Represent a number
+ * 
+ * Import <a target="_blank" 
+ href="https://github.com/yahoo/jafar/blob/master/packages/react-components/src/edit/Number/Number.jsx">
+ Number</a> from '@jafar-org/react-components/edit/Number'
+ */
 export default class Number extends React.Component {
-  static propTypes = Object.assign({}, propTypes, {
+  static propTypes = {
     value: PropTypes.number,
-  })
+    state: PropTypes.shape({
+      placeholder: PropTypes.string,
+      min: PropTypes.number,
+      max: PropTypes.number,
+      step: PropTypes.number,
+      endAdornment: PropTypes.string,
+      startAdornment: PropTypes.string,
+    }),
+    disabled: PropTypes.bool,
+    invalid: PropTypes.bool,
+    onValueChange: PropTypes.func.isRequired,
+  };
 
-  static defaultProps = Object.assign({}, defaultProps, {
+  static defaultProps = {
     value: undefined,
     state: {
       placeholder: '',
@@ -23,7 +40,9 @@ export default class Number extends React.Component {
       endAdornment: undefined,
       startAdornment: undefined,
     },
-  })
+    disabled: false,
+    invalid: false,
+  };
 
   render() {
     const value = this.props.value === undefined ? '' : this.props.value; // Fixing Material controlled VS uncontrolled issue
