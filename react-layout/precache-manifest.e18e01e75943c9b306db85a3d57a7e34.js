26d37cf1d4c9b2040a225509687ea5f98e624c3e
Gal Havivi <gal.havivi@oath.com>
2020-02-04 23:33:11 +0200

    Updates

--- react-layout/precache-manifest.e18e01e75943c9b306db85a3d57a7e34.js
+++ react-layout/precache-manifest.e18e01e75943c9b306db85a3d57a7e34.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "42a5e5648c62691b55e6c8f15387d744",
+    "revision": "5929cf2733ae7bea15f33afcee42340b",
     "url": "https://yahoo.github.io/jafar/react-layout/index.html"
   },
   {
-    "revision": "ebf90b767322dcd520a7",
+    "revision": "f042b9d3676d0a2b91d3",
     "url": "https://yahoo.github.io/jafar/react-layout/static/css/main.6ea54503.css"
   },
   {
-    "revision": "ebf90b767322dcd520a7",
-    "url": "https://yahoo.github.io/jafar/react-layout/static/js/main.e5157b51.js"
+    "revision": "f042b9d3676d0a2b91d3",
+    "url": "https://yahoo.github.io/jafar/react-layout/static/js/main.de460cd5.js"
   },
   {
-    "revision": "b0f5c9519bc95c0c31fd10fc7f96092d",
-    "url": "https://yahoo.github.io/jafar/react-layout/static/js/main.e5157b51.js.LICENSE"
+    "revision": "b371cdc871ccd947058ba47cda8966e9",
+    "url": "https://yahoo.github.io/jafar/react-layout/static/js/main.de460cd5.js.LICENSE"
   },
   {
     "revision": "1c1a52059bdc91a8a6e4ac28bea53710",
--- react-layout/service-worker.js
+++ react-layout/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://yahoo.github.io/jafar/react-layout/precache-manifest.930e7445700e71d90002be417db663bb.js"
+  "https://yahoo.github.io/jafar/react-layout/precache-manifest.e18e01e75943c9b306db85a3d57a7e34.js"
 );
 
 self.addEventListener('message', (event) => {
