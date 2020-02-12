412039cdc1fa95db158206e011c3d2e0edf13344
Gal Havivi <galhavivi@gmail.com>
2020-02-12 22:08:21 +0200

    fix-demos-scrollto-selected-menu-item (#21) (#4)
    * fix-demos-scrollto-selected-menu-item
    
    * Update Root.e2e.js

--- packages/react-form/src/website/Root.e2e.js
+++ packages/react-form/src/website/Root.e2e.js
@@ -76,15 +76,15 @@ async function testHomepageNavigation(page) {
   expect(itemsBeforeOpen).toHaveLength(10);
 
   // verify child items not appear yet
-  let firstDemo = await page.$('[id="/basics/basic"][role="button"]');
+  let firstDemo = await page.$('[item-id="/basics/basic"][role="button"]');
   expect(firstDemo).toBeFalsy();
 
   // open parent item of "basics"
-  await page.click('[id="/basics"][role="button"]');
+  await page.click('[item-id="/basics"][role="button"]');
   await page.waitFor(ANIMATION_DURATION);
 
   // verify its sub items appear on the screen
-  firstDemo = await page.$('[id="/basics/basic"][role="button"]');
+  firstDemo = await page.$('[item-id="/basics/basic"][role="button"]');
   expect(firstDemo).toBeTruthy();
 
   // verify that home is presented and not any example
@@ -94,7 +94,7 @@ async function testHomepageNavigation(page) {
   expect(home).toBeTruthy();
 
   // click first example item
-  await page.click('[id="/basics/basic"][role="button"]');
+  await page.click('[item-id="/basics/basic"][role="button"]');
 
   // verify that home is not presented and first example is presented
   demoContainer = await page.$('#example-container');
@@ -132,7 +132,7 @@ async function testReactDemos(page) {
   // run demos e2e
   await asyncForEach(menu.slice(1), async (parentItem) => {
     // open parent
-    const parentItemSelector = `#\\${parentItem.id}`;
+    const parentItemSelector = `[item-id="${parentItem.id}"]`;
     await page.click(parentItemSelector);
     await page.waitFor(ANIMATION_DURATION);
 
@@ -165,7 +165,7 @@ async function testDemos(page, demos) {
 
 async function testDemo(page, demoId, demoWait) {
   // click on the demo item om the left sidebar
-  const itemSelector = `#${demoId.replace(/\//g, '\\/')}`;
+  const itemSelector = `[item-id="${demoId.replace(/\//g, '\\/')}"]`;
   await page.click(itemSelector);
   
   // verify html tab
