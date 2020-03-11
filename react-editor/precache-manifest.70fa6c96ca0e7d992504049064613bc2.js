db30904ef5cb96f3413893a593f31164643bbb59
Gal Havivi <gal.havivi@oath.com>
2020-03-11 17:56:08 +0200

    Updates

---
+++ react-editor/precache-manifest.70fa6c96ca0e7d992504049064613bc2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc65b2f37b7e5009d28fac191041a7d7",
+    "url": "https://yahoo.github.io/jafar/react-editor/index.html"
+  },
+  {
+    "revision": "529b9f1fdb2dde4b2aed",
+    "url": "https://yahoo.github.io/jafar/react-editor/static/js/main.cfe3ebbe.js"
+  },
+  {
+    "revision": "a46f5c5ab1fd9a82f043895b6d4e59c1",
+    "url": "https://yahoo.github.io/jafar/react-editor/static/js/main.cfe3ebbe.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c1a52059bdc91a8a6e4ac28bea53710",
+    "url": "https://yahoo.github.io/jafar/react-editor/static/media/jafar.1c1a5205.svg"
+  }
+]);
