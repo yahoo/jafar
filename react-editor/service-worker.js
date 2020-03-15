f2ffd07319a914ce8e24944b433893e169db5755
Gal Havivi <gal.havivi@oath.com>
2020-03-15 12:44:30 +0200

    Updates

--- react-editor/service-worker.js
+++ react-editor/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://yahoo.github.io/jafar/react-editor/precache-manifest.9e1ff41d670e8919980d995af56af4b8.js"
+  "https://yahoo.github.io/jafar/react-editor/precache-manifest.61a2c26a8543f2cbe3be1f523c9c94c3.js"
 );
 
 self.addEventListener('message', (event) => {
