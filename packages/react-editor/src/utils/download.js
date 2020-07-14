cf64ae1d337bd28f58a2d0f2b29cbab1f00369c4
Gal Havivi <gal.havivi@oath.com>
2020-07-14 09:23:01 +0300

    update

--- packages/react-editor/src/utils/download.js
+++ packages/react-editor/src/utils/download.js
@@ -70,115 +70,53 @@ const addSectionsFile = (sections, folder) => {
   folder.file('sections.js', sectionsStr);
 }
 
+const getObjectStr = (obj, x) => (obj[x] !== undefined ? `\n          ${x}: '${obj[x]}',` : '');
+
+const getObjectFn = (obj, x) => (obj[x] ? `\n          ${x}: () => { /* TODO: ${obj[x]} */ },` : '');
+
+const getActions = (actions) => `[${actions.map(action => {
+  return '{' 
+    + getObjectStr(action, 'label')
+    + getObjectStr(action, 'type')
+    + getObjectFn(action, 'exclude')
+    + getObjectFn(action, 'disable')
+    + getObjectFn(action, 'onClick')
+    + '\n        }'
+}).join(', ')}]`;
+
+const getItem = (item) => '{' 
+  + (item.title !== undefined ? `\n        title: '${item.title}',` : '')
+  + (item.layout !== undefined ? `\n        layout: '${item.layout}',` : '')
+  + (item.size !== undefined ? `\n        size: ${item.size},` : '')
+  + '\n        sections,'
+  + (item.mainActions ? `\n        mainActions: ${getActions(item.mainActions)},` : '')
+  + (item.optionsActions ? `\n        optionsActions: ${getActions(item.optionsActions)},` : '')
+  + '\n      },';
+
 const addItemFile = (item, folder) => {
   const itemStr = `
 import React from 'react';
 import { FormContext, createForm } from '@jafar/react-form';
-import { iterateSections, getSectionComponentBoxes } from '@jafar/react-layout/Section/utils';
-import { iterateBoxes } from '@jafar/react-layout/Box/utils';
-import SaveIcon from '@material-ui/icons/Save';
 import Item from '@jafar/react-layout/Item';
-import FormErrors from '@jafar/react-components/view/FormErrors';
-import form from './form';
+import form from '../../form';
 import sections from './sections';
 
-class Demo extends React.Component {
+class ItemLayout extends React.Component {
   static contextType = FormContext;
 
   constructor(props) {
     super(props);
 
-    // apply exclude condition to sections - exclude section if all of its fields are excluded
-    iterateSections(sections, this.applySectionsExclude);
-
     this.state = {
-      item: {
-        title: 'Employee',
-        layout: 'scroll',
-        size: 4,
-        sections,
-        mainActions: [{
-          label: 'Cancel',
-          type: 'tertiary',
-          onClick: () => console.log('Cancel', this.context.model.data), // eslint-disable-line
-        }, {
-          label: 'Save & Close',
-          type: 'secondary',
-          disable: () => this.context.model.invalid,
-          onClick: () => console.log('Save & Close', this.context.model.data), // eslint-disable-line
-        }, {
-          label: 'Save',
-          type: 'primary',
-          icon: SaveIcon,
-          disable: () => this.context.model.invalid,
-          onClick: () => console.log('Save & Close', this.context.model.data), // eslint-disable-line
-          popover: {
-            title: 'Handle Fields',
-            open: () => this.context.model.invalid,
-            component: FormErrors,
-            props: { 
-              onClickField: this.onClickField,
-            },
-          },
-        }],
-        optionsActions: [{
-          label: 'Archive',
-          onClick: () => {},
-        }, {
-          label: 'History',
-          onClick: () => {},
-          exclude: () => this.context.model.data.department === 'HR',
-        }, {
-          label: 'Report To HR',
-          onClick: () => {},
-          disable: () => this.context.model.data.department === 'HR',
-        }, {
-          label: 'Delete',
-          onClick: () => {},
-        }],
-      },
+      item: ${getItem(item)}
     };
   }
 
-  render() {
-    return (<Item {...this.state.item} />);
-  }
-
-  onClickField = (fieldId) => {
-    this.setState({
-      item: {
-        ...this.state.item,
-        selected: { sectionId: this.getSectionId(fieldId), elementId: fieldId },
-      },
-    });
-  }
-
-  getSectionId = (fieldId) => {
-    let sectionId;
-
-    iterateSections(sections, (section) => {
-      iterateBoxes(section.boxes, (box) => {
-        if (box.props && box.props.id === fieldId) {
-          sectionId = section.id;
-        }
-      });
-    });
-
-    return sectionId;
-  }
-
-  applySectionsExclude = (section) => {
-    section.exclude = () => {
-      const boxes = getSectionComponentBoxes(section);
-      const fieldIds = boxes.map(box => box.props.id);
-      const excluded = !fieldIds.find(fieldId => !this.context.model.fields[fieldId].excluded);
-      return excluded;
-    };
-  }
+  render = () => (<Item {...this.state.item} />);
 }
 
-export default createForm(form)(Demo);
-  `;
+export default createForm(form)(ItemLayout);
+`;
 
   folder.file('Item.jsx', itemStr);
 }
