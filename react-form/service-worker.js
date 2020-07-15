d828a3538286b5620a1a0f0ea38db3348b88abb1
Gal Havivi <gal.havivi@oath.com>
2020-07-15 18:25:25 +0300

    Updates

--- react-form/service-worker.js
+++ react-form/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://yahoo.github.io/jafar/react-form/precache-manifest.5063269474db58b884612051e83e9c4a.js"
+  "https://yahoo.github.io/jafar/react-form/precache-manifest.b225a6e4b8f6e9fb8b0a7cf7303086a2.js"
 );
 
 self.addEventListener('message', (event) => {
