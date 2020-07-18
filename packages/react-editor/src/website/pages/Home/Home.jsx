c2f8b5fe782f41dbf8634f589a54ffd8fc39fcce
Gal Havivi <gal.havivi@oath.com>
2020-07-18 20:06:33 +0300

    update

--- packages/react-editor/src/website/pages/Home/Home.jsx
+++ packages/react-editor/src/website/pages/Home/Home.jsx
@@ -39,15 +39,23 @@ export default withRouter(({ history }) => {
   return (<Wrapper aria-label="home-page">
     <h1>Editor</h1>
     <p>
-      We created a simple UI form editor - which you can use to create the form <Link
-        href="https://yahoo.github.io/jafar/docs/arguments.html#model">model configuration</Link> and <Link
-        href="https://yahoo.github.io/jafar/docs/react-layout.html">layout configuration</Link>, to help you get started. 
-      These form configurations demos are saved on local storage.
-      You can create form models, and download them (you might need to supply <Link
-        href="https://yahoo.github.io/jafar/docs/arguments.html#resources">resources</Link> such as actual components
-      if defined prior to use it in the Form class / component). In addition you can clone Jafar repository and
-      change / adapt the Form Editor to your system's needs (located in the "react-editor" package).
+      A simple UI form editor - which you can use to create the form's <Link
+        href="https://yahoo.github.io/jafar/docs/arguments.html#model">model</Link> and <Link
+        href="https://yahoo.github.io/jafar/docs/react-layout.html">layout</Link> configurations, to help you get started. 
+      These form configurations are saved on your local storage.
     </p>
+    <h3>Features</h3>
+    <ul>
+      <li>Create form configuration</li>
+      <li>Download form configuration as jsons and as files (you might need to supply custom <Link
+        href="https://yahoo.github.io/jafar/docs/arguments.html#resources">resources</Link> such as actual components,
+        custom validators and more - if defined in the form model, prior to passing it to the Form class / component)</li>
+      <li>Create common fields in 'Fields Library'</li>
+      <li>Add common fields to a form using a reference (i.e when a library field is updated - all referenced fields are updated as well), 
+        or clone (i.e a full copy of the library field which will not be affected when the library field updates)</li>
+      <li>Clone Jafar repository and adapt the Form Editor to your system's needs (located in the "react-editor" package)</li>
+    </ul>
+    <h3>Get Started</h3>
     <Actions aria-label="actions">
       {
         actions.map(action => (<Action key={action.id} aria-label={action.id} onClick={action.onClick}>
