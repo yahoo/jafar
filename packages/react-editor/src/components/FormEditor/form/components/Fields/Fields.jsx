1c1827ac7b9d0b626413185f4e75fdd6ad2b426a
Gal Havivi <gal.havivi@oath.com>
2020-07-18 15:29:53 +0300

    update

--- packages/react-editor/src/components/FormEditor/form/components/Fields/Fields.jsx
+++ packages/react-editor/src/components/FormEditor/form/components/Fields/Fields.jsx
@@ -8,24 +8,36 @@ import cloneDeep from 'lodash/cloneDeep';
 import { FormContext } from '@jafar/react-form';
 import { FieldEditor } from '../../../../index';
 import Grid from '../../../../Grid';
+import Dialog from '../../../../Dialog';
 import { downloadJson } from '../../../../../utils/download'; 
 import columns from './columns';
 import rowActions from './row-actions';
 import headerActions from './header-actions';
-import * as Styled from './Styled';
+import { FieldEditorWrapper, GridWrapper } from './Styled';
 
 
 const Fields = ({ value = {}, state = {}, onValueChange, onStateChange }) => {
   const parentForm = useContext(FormContext);
-  const { editingField } = state;
+  const { editingField, showModal } = state;
   const setEditingField = editingField => onStateChange({ ...state, editingField });
+  const setShowModal = name => onStateChange({ ...state, showModal: name });
 
-  const add = () => {
-    setEditingField({ field: {} });
+  const add = () => setEditingField({ field: {} });
+
+  const save = ({ data }) => {
+    const fieldId = data.id;
+    const field = { ...data };
+    delete field.id;
+    const newValue = { ...value };
+    newValue[fieldId] = field;
+    onValueChange(newValue);
+    setEditingField();
   };
 
-  const addFromLibrary = () => {
-    // todo
+  const addFromLibrary = () => setShowModal('library');
+
+  const saveFromLibrary = () => {
+    
   };
 
   const duplicate = ({ fieldId }) => {
@@ -44,38 +56,38 @@ const Fields = ({ value = {}, state = {}, onValueChange, onStateChange }) => {
     onValueChange(newValue);
   };
 
-  const save = ({ data }) => {
-    const fieldId = data.id;
-    const field = { ...data };
-    delete field.id;
-    const newValue = { ...value };
-    newValue[fieldId] = field;
-    onValueChange(newValue);
-    setEditingField();
-  };
-
   const data = Object.keys(value).sort().map(fieldId => ({ fieldId, field: value[fieldId] }));
   const parentModel = parentForm.model.data.model || {};
   const fieldsLibrary = parentForm.model.context.fieldsLibrary;
   
   return (<>
     {
-      editingField && <Styled.FieldEditorWrapper aria-label="field-editor"><FieldEditor 
+      editingField && <FieldEditorWrapper aria-label="field-editor"><FieldEditor 
         formId={parentModel.id} 
         fieldId={editingField.fieldId} 
         field={editingField.field} 
         fieldIds={Object.keys(parentModel.fields || {})} 
         onCancel={setEditingField} 
         onSave={save} 
-      /></Styled.FieldEditorWrapper>
+      /></FieldEditorWrapper>
     }
-    <Styled.GridWrapper>
+    {
+      showModal === 'library' && <Dialog 
+        open={true}
+        title="Add Field From Library"
+        confirmText="Add"
+        onConfirm={saveFromLibrary} 
+        onCancel={setShowModal}>
+        <div>hii</div>
+      </Dialog>
+    }
+    <GridWrapper>
       <Grid
         columns={columns({ setEditingField })}
         data={data}
         headerActions={headerActions({ add, addFromLibrary, fieldsLibrary })}
         rowActions={rowActions({ setEditingField, duplicate, download, remove })} />
-    </Styled.GridWrapper> 
+    </GridWrapper> 
   </>);
 };
 
